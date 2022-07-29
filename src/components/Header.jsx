305e167708196a452a0eb25336d3c4465b57c638
Tuquina <fernandotuquina@gmail.com>
2022-07-29 13:51:13 -0300

    Primer Commit Aplicación Versión 0.1 Pantalla principal, agregado y edición de gastos con modal

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
+
+const header = ( {
+    gastos,
+    presupuesto, 
+    setPresupuesto, 
+    isValidPresupuesto, 
+    setIsValidPresupuesto
+} ) => {
+  return (
+    <header>
+        <h1>Planificador de gastos</h1>
+
+        {isValidPresupuesto ? (
+            <ControlPresupuesto 
+                gastos={gastos}
+                presupuesto={presupuesto}
+            />
+        ) : (
+            <NuevoPresupuesto 
+            presupuesto={presupuesto}
+            setPresupuesto={setPresupuesto}
+            setIsValidPresupuesto={setIsValidPresupuesto}
+        />    
+        )}
+        
+    </header>
+  )
+}
+
+export default header
